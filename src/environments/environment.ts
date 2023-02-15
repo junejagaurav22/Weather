c664e0dad1e7dea46389fd85461b52aebc429143
Gaurav Juneja <junejagaurav22@gmail.com>
2023-02-16 01:13:18 +0530

    Nav-bar ready

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,12 @@
+import { Environment } from "./Environment.interface";
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+
+export const environment : Environment= {
+  production: false,
+  configUrl: 'https://api.api-ninjas.com/v1/weather',
 };
 
 /*
